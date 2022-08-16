15b86a8cb92c3e6e6cde06bf411d34c1fab8d06c
ugotman <ugochuxy@gmail.com>
2022-08-16 14:46:05 -0700

    sinly listing

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,22 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_listint - prints all the elements of a linked list
- * @h: head of the list
+ * print_listint - prints all the elements of a listint_t list
  *
- * Return: the number of nodes
+ * @h: pointer to the start of the list
+ * Return: number of nodes in the list
  */
+
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *cursor = h;
-	size_t count = 0;
+	size_t count_nodes = 0;
 
-	while (cursor != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", cursor->n);
-		count += 1;
-		cursor = cursor->next;
+		printf("%d\n", h->n);
+		count_nodes++;
+		h = h->next;
 	}
-	return (count);
+	return (count_nodes);
 }
